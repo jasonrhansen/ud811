c8010b7c659dbbac624db5ab115103f2e33c0bde
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2017-11-13 13:57:44 -0700

    Save and load selected cities with localForage

--- 1-12-skeleton/scripts/app.js
+++ 1-12-skeleton/scripts/app.js
@@ -15,6 +15,35 @@
     daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   };
 
+  var injectedCities = [{key: 'newyork', label: 'New York, NY'}];
+
+  var injectedForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
 
   /*****************************************************************************
    *
@@ -41,6 +70,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -150,4 +180,23 @@
     });
   };
 
+  app.saveSelectedCities = function() {
+    localforage.setItem('selectedCities', app.selectedCities);
+  };
+
+  localforage.getItem('selectedCities').then(function(value) {
+    if (value !== null) {
+      app.selectedCities = value;
+      app.selectedCities.forEach(function(city) {
+        app.getForecast(city.key, city.label);
+      });
+    } else {
+      app.selectedCities = [
+        {key: injectedForecast.key, label: injectedForecast.label}
+      ];
+      localforage.setItem('selectedCities', injectedCities);
+      app.saveSelectedCities();
+      app.updateForecastCard(injectedForecast);
+    }
+  });
 })();
